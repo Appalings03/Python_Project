b51208ab370925e4ddc4424ea4e35fbaa5354553
Appalings03 <martin.biname@gmail.com>
2026-04-03 23:38:09 +0200

    Basic use for variables

---
+++ Entrainnement/variable.py
@@ -0,0 +1,20 @@
+#commentaire
+
+age = 30
+print(age)
+
+age = "trente"
+print(age)
+
+print("Hello world " +  str(30))
+
+def uneFonction():
+    age = 18
+    print(age)
+
+uneFonction()
+print(age)
+#
+#del age
+print(age)
+
